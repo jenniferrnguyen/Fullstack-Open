1b769ee54552a2bac43885e83b202d8819560d97
jennifernguyen <jennifer.t.nguyen.02@gmail.com>
2023-07-21 23:07:03 -0700

    finished exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,6 +9,16 @@ const Header = (prop) => {
 const Content = (prop) => {
   return (
     <>
+      <Part part={prop.part1} exercises={prop.exercises1}/>
+      <Part part={prop.part2} exercises={prop.exercises2}/>
+      <Part part={prop.part3} exercises={prop.exercises3}/>
+    </>
+  )
+}
+
+const Part = (prop) => {
+  return (
+    <>
       <p>{prop.part} {prop.exercises}</p>
     </>
   )
@@ -52,11 +62,14 @@ const App = () => {
       <p>
         {part3} {exercises3}
       </p>
-      */}
+      
 
       <Content part={part1} exercises={exercises1}/>
       <Content part={part2} exercises={exercises2}/>
       <Content part={part3} exercises={exercises3}/>
+      */}
+
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       
       {/* 
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
